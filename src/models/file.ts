9f57fe974b414540d34bed96021b2539f59108eb
Tabrez Shaikh <tabrezsh001@gmail.com>
2025-07-17 14:38:02 +0530

    Add support for webP, png, gif

--- src/models/file.ts
+++ src/models/file.ts
@@ -12,11 +12,32 @@ const FileSchema = new Schema<IFile>({
   fileName: { type: String, required: true},
   filePath: { type: String, required: true},
   fileSizeInNumber: { type: Number, default: 0},
-  fileType: { type: String, required: true, enum: ["image/png", "image/jpeg", "application/pdf", "application/postscript"] 
-  },
+  fileType: { type: String, required: true, enum: [
+    // Image formats
+    "image/png", 
+    "image/jpeg", 
+    "image/jpg", 
+    "image/gif", 
+    "image/webp",
+    "image/svg+xml",
+    // Design file formats
+    "application/pdf",
+    "application/postscript",
+    "image/vnd.adobe.photoshop",
+    "application/x-photoshop",
+    "image/photoshop",
+    "image/x-photoshop",
+    "application/photoshop",
+    "image/psd",
+    "application/illustrator",
+    "application/x-illustrator",
+    "image/x-eps",
+    "application/eps",
+    "application/x-eps"
+  ]},
   uploadAt: { type: Date, default: Date.now },
 })
 
-const FileModel = mongoose.model<IFile>('File', FileSchema);
+const FileModel = mongoose.models.File || mongoose.model<IFile>('File', FileSchema);
 
 export default FileModel;
